bb566d7018cd44e38beacbe686a5a9e138733c35
rasmuslundmark <rasmuslbus@gmail.com>
2024-01-26 17:41:24 +0100

    Uppdaterade Listener.py med kod och kommentarer. Integrerade med insert_image_data_test.py.

--- app/db/Listener.py
+++ app/db/Listener.py
@@ -2,15 +2,23 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 
+# import insert_image_data_test as insert_image
+
+# Todo: Parsern måste fungera för att automatiskt läggas in i db, fixa rätt path i servern
+#Event Handler that checks if a file is added. Then it shall parse the filename and add it to the db.
 class MyHandler(FileSystemEventHandler):
     def on_created(self, event):
-        if event.is_directory:
-            print("Directory created")
-        else:
-            print("File added")
+        if not event.is_directory:
+            print("File added", event.src_path)
+            # insert_image.add_data(event.src_path, "andra parametern till parsern?") Här skall den läggas in automatiskt in i databasen med hjälp av parsern.
+
+    def on_deleted(self, event):
+        print("File deleted")  # Behövs det upptäckas ifall filer tas bort? Isåfall skall de tas bort från db?
 
 
-def Listener(path):
+# Function to observe the FTP path
+def Listener():
+    path = "./test/" # fixa rätt path i server-miljö
     event_handler = MyHandler()
     observer = Observer()
     observer.schedule(event_handler, path=path, recursive=False)
@@ -18,6 +26,5 @@ def Listener(path):
     print("Listener:", path)
     observer.join()
 
-if __name__ == "__main__":
-    folder_path = "" # Byt ut detta mot rätt path.
-    Listener(folder_path)
+
+Listener()
